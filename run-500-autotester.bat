4275b267290ffc159aa94c33051e07c451f7a293
Keith Low <46418515+fairyinabottle4@users.noreply.github.com>
2021-11-07 22:01:22 +0800

    Merge Affects/* system tests  (#257)
    * Add affects/* functional tests
    
    * Commit passing affects functional tests
    
    * Add passing algo tests for affects
    
    * Add 100 line stress tests. One TLE, should be resolved through optimizations later
    
    * Add 250 line stress tests with some failures
    
    * Add more stress tests as well as 500 line source. Added batch files so that the tests can be run on autotester concurrently
    
    * Add NextBip functional tests
    
    * Amend a couple of queries
    
    * Remove stress tests and extensions tests
    
    * Add updated NextBip/* tests
    
    * make ci run
    
    * Delete NextBip tests which were accidentally added
    
    * Add AffectsT tests
    
    * fix shit
    
    * Fix AffectsTFunctionalTest

---
+++ run-500-autotester.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+Team02\Code02\build_win\x86-Debug\src\autotester\autotester.exe "Team02\Tests02\StressTest500Lines_source.txt" "Team02\Tests02\StressTest500Lines_queries.txt" "Team02\Tests02\XmlFiles\StressTest500Lines_out.xml"
+PAUSE
