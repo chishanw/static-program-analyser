5b2ca136df9758d5b049ec059679d4c44ba811de
Keith Low <46418515+fairyinabottle4@users.noreply.github.com>
2021-11-07 23:39:49 +0800

    Merge stress tests (#259)
    * Add all stress tests
    
    * add batch files for ease of testing
    
    * Fix some test cases

---
+++ run-stress-100Ultimate.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+Team02\Code02\build_win\x86-Debug\src\autotester\autotester.exe "Team02\Tests02\StressTest100LinesUltimate_source.txt" "Team02\Tests02\StressTest100LinesUltimate_queries.txt" "Team02\Tests02\XmlFiles\StressTest100LinesUltimate_out.xml"
+PAUSE
